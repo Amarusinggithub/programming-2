4bdb2b04ff9adeec65d80bc14d0ad09b8c4fbf49
Amarusinggithub <loveamar285@gmail.com>
2026-01-26 09:20:29 -0500

    python implementation of worksheet 4-Q-4

--- src/main.py
+++ src/main.py
@@ -1,10 +1,30 @@
-account_balance= 10000
+'''
+Author: Amar Campbell -2501977
+Date: 26/01/2026
+Description: Programming-2 Worksheet-1-Q-4
+'''
+# Constants
+FIRST_HUNDRED= 0.50 
+NEXT_HUNDRED= 0.75 
+MORE_HUNDRED= 1.00 
 
-withdrawal = int(input("Enter your withdrawal amount: "))
+# variables
+total_bill=0
 
-if withdrawal > account_balance or withdrawal <0:
-    print("You do not have sufficient balance to complete this transaction")
+
+print("Electricity Limited\n")
+
+# Prompt user for units
+units=int(input("Enter the number of units used:\n"))
+
+tier= units-100
+
+if units <= 100:
+    total_bill=units*FIRST_HUNDRED
+elif units <= 200:
+    total_bill = (100*FIRST_HUNDRED)+ (units-100) * NEXT_HUNDRED
 else:
-    account_balance = account_balance-withdrawal
-    print("You withdrawed: " + str(withdrawal))
-    print("Your final balance is: " + str(account_balance))
+    total_bill = (100 * FIRST_HUNDRED) + (100 * NEXT_HUNDRED) + (units -200)* MORE_HUNDRED
+
+
+print(f"This is your total bill:${total_bill}")
