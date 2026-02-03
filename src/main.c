077005cba197894f9a988d2808126e14067aba57
Amarusinggithub <loveamar285@gmail.com>
2026-02-03 08:29:13 -0500

    modulus

--- src/main.c
+++ src/main.c
@@ -1,39 +1,40 @@
 /*
 Author: Amar Campbell- 2501977
 Data: 27/01/2026
-Description: Worksheet 2 Question 3
+Description: Worksheet 2 Question 4
+
+ A fast-food restaurant allows customers to place multiple orders. Each item costs $500.
+Write a program that will repeatedly ask if the customer wants to order another item
+(Y/N). Calculate and display the total cost when ordering is complete.
+
 */
+
 
 #include <stdio.h>
 
 
+int calculateModulus(int  num1, int num2);
 
 
 int main (){
 
-float totalPayroll=0.00;
-float weeklySalary=0.00;
-int aboveThreshold=0;
-const float THRESHOLD=80000;
 
-for(int i =0; i<40;i++){
-printf("Please enter employee number %d weekly salary:\n $", i+1);
-scanf("%f", &weeklySalary );
-totalPayroll+=weeklySalary;
-if (weeklySalary< THRESHOLD){
-    continue;
-}
-aboveThreshold+=1;
-}
+calculateModulus(100, 301);
 
-printf("This is the total payroll cost: $%.2f\n", totalPayroll);
-printf("The number of employees with salary above $80,000: %d\n", aboveThreshold);
+
 
 return 0;
 
-
 }
 
+int calculateModulus(int  num1, int num2){
+while (num1>=num2){
+    num1=num1-num2;
+}
+
+   return num1;
+
+}
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,27 +1,13 @@
-'''
+"""
 Author: Amar Campbell- 2501977
 Data: 27/01/2026
-Description: Worksheet 2 Question 3
-'''
+Description: Worksheet 2 Question 4
 
-totalPayroll=0.00
-weeklySalary=0.00
-aboveThreshold=0
-THRESHOLD=80000
+A fast-food restaurant allows customers to place multiple orders. Each item costs $500.
+Write a program that will repeatedly ask if the customer wants to order another item
+(Y/N). Calculate and display the total cost when ordering is complete.
 
-for i in range (40):
-    weeklySalary=float(input(f"Please enter employee number {i+1} weekly salary:\n $"))
-    totalPayroll+=weeklySalary
-    if weeklySalary< THRESHOLD:
-        continue
-    aboveThreshold+=1
-
-
-print(f"This is the total payroll cost: ${totalPayroll}\n")
-print(f"The number of employees with salary above $80,000: {aboveThreshold}\n")
+"""
 
 
 
-        
-        
-        
