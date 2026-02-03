dc1dc5d49ccc14378ba1310ab6a07d2584b88f95
Amarusinggithub <loveamar285@gmail.com>
2026-02-03 09:37:05 -0500

    calculate average

--- src/main.c
+++ src/main.c
@@ -1,28 +1,37 @@
 /*
 Author: Amar Campbell- 2501977
-Data: 27/01/2026
-Description: Worksheet 2 Question 4
+Data: 03/02/2026
 */
 
 #include <stdio.h>
 
 
-void greetings (char name []);
+float average (float grades []);
 
 int main (){
+float grades [3];
 
-char name[20];
-printf("Enter a number:\n");
+for(int i =0; i<3; i++){
+printf("Enter your test score:\n");
+scanf("%f", &grades[i] );
+}
 
-scanf("%s", name);
-greetings(name);
+float mean=average(grades);
+
+printf("This is your average: %.2f\n", mean);
 
 return 0;
 }
 
-void greetings(char name []){
+float average(float grades []){
+float average=0.00;
+float sum =0.00;
 
-printf("hello %s\n", name);
+for (int i=0; i<3; i++){
+    sum+=grades[i];
+}
+average= sum/3;
+return average;
 
 }
 
--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 """
 Author: Amar Campbell- 2501977
-Data: 27/01/2026
-Description: Worksheet 2 Question 4
-
-A fast-food restaurant allows customers to place multiple orders. Each item costs $500.
-Write a program that will repeatedly ask if the customer wants to order another item
-(Y/N). Calculate and display the total cost when ordering is complete.
-
+Data: 03/02/2026
 """
+grades=[]
+
+def average(grades)-> float:
+    total=0.00
+    for i in range(3):
+        total +=grades[i]
+    average = total / 3
+    return average
+
+for i in range (3):
+    grades.append(float(input("Enter your test score:\n")))
 
 
+mean= average(grades)
 
+print(f"this is your average: {mean}\n")
