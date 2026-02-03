371beb11829f0d5b88011d80ea9fda190e05d4cb
Amarusinggithub <loveamar285@gmail.com>
2026-02-03 08:45:51 -0500

    multiply two numbers

--- src/main.c
+++ src/main.c
@@ -2,37 +2,23 @@
 Author: Amar Campbell- 2501977
 Data: 27/01/2026
 Description: Worksheet 2 Question 4
-
- A fast-food restaurant allows customers to place multiple orders. Each item costs $500.
-Write a program that will repeatedly ask if the customer wants to order another item
-(Y/N). Calculate and display the total cost when ordering is complete.
-
 */
-
 
 #include <stdio.h>
 
 
-int calculateModulus(int  num1, int num2);
-
+int multiplication(int  num1, int num2);
 
 int main (){
 
-
-calculateModulus(100, 301);
-
-
+printf("%d\n",multiplication(10, 30));
 
 return 0;
-
 }
 
-int calculateModulus(int  num1, int num2){
-while (num1>=num2){
-    num1=num1-num2;
-}
+int multiplication(int  num1, int num2){
 
-   return num1;
+return num1*num2;
 
 }
 
