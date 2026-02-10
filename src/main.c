be40018eee090b2475efea692051ced52ab48e94
Amarusinggithub <loveamar285@gmail.com>
2026-02-10 18:07:58 -0500

    CAI

--- src/main.c
+++ src/main.c
@@ -1,40 +1,103 @@
 /*
 Author: Amar Campbell- 2501977
-Data: 03/02/2026
 */
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 
+float calculatePercentage(int * correct, int numQues);
+int generateRand(int max, int min);
+int multiplication(int num1,int num2);
+void checkAns(int random, int result, int ans,int* correct);
 
-int factorial (int num);
+
 int main (){
-    int num;
-    char ans;
-while (true){
-    printf("Please enter a number:\n");
-    scanf("%d",&num );
-    int num2= factorial(num);
+    srand(time(0));
+    int num1, num2,ans,result,random,numQues=10, correct=numQues;
 
-    printf("This is the factorial: %d\n",num2);
+for (int i=0; i<10;i++){
 
-    printf("\nDo you want to continue (Y/N)?\n");
-    scanf(" %c",&ans );
+random=generateRand(4,1);
+num1= generateRand(10,1);
+num2=generateRand(10,1);
+result=multiplication( num1,  num2);
 
-    if(ans=='n' || ans=='N'){
-        break;
-    }
+printf("\nHow much is %d times %d?\n ", num1, num2);
+scanf("%d", &ans);
 
+checkAns( random, result, ans,&correct);
+    
 }
+
+float percent= calculatePercentage(&correct,  numQues);
+printf("Your score is %d/%d which is %.1f%%\n.",correct, numQues, percent);
+printf("\nThank you for participating.\n");
+correct=numQues;
 
 return 0;
 }
 
-int factorial(int num){
-    if (num==0){
-        return 1;
-    }else{
-        return num*factorial(num-1);
+
+int generateRand(int max, int min){
+    return rand()%(max-min+1)+min;
+}
+
+int multiplication(int num1,int num2){
+    return num1 * num2;
+}
+
+void checkAns(int  random, int  result, int  ans,int* correct){
+    if(result==ans){
+    switch(random){
+        case 1:
+        printf("Very good!");
+        break;
+        case 2:
+        printf("\nExcellent!\n");
+        break;
+        case 3:
+        printf("\nNice work!\n");
+        break;
+        case 4:
+        printf("\nKeep up the good work!\n");
+        break;
+        default:
+            printf("\nVery good!\n");
+            break;
+    }
+
+}else{
+
+    switch(random){
+        case 1:
+        *correct--;
+        printf("\nNo. Please try again.\n");
+        break;
+        case 2:
+        *correct--;
+        printf("\nWrong. Try once more.\n");
+        break;
+        case 3:
+        *correct--;
+        printf("\nDon't give up!\n");
+        break;
+        case 4:
+        *correct--;
+        printf("\nNo. Keep trying.\n");
+        break;
+        default:
+        *correct--;
+        printf("\nInvalid Input. Please try again\n");
+        break;
     }
 }
+}
+
+float calculatePercentage(int * correct, int numQues){
+return (*correct/ numQues)*100;
+}
+
+
--- src/main.py
+++ src/main.py
@@ -1,21 +1,63 @@
 """
 Author: Amar Campbell- 2501977
-Data: 03/02/2026
 """
 
+import random
 
-def factorial(num):
-    if num==0:
-        return 1
+num_ques = 10
+correct = num_ques
+
+def multiplication( num1:int, num2:int):
+    return num1 * num2
+
+
+def calculate_percentage( ) -> float:
+    return correct / num_ques
+
+
+def check_ans(  rand:int,   result:int,   ans:int):
+    global correct
+    if result==ans :
+        match rand:
+            case 1:
+                print("Very good!")
+            case 2:
+                print("\nExcellent!\n")
+            case 3:
+                print("\nNice work!\n")
+            case 4:
+                print("\nKeep up the good work!\n")
+            case _:
+                print("\nVery good!\n")
     else:
-        return factorial(num-1)*num
+        match rand:
+            case 1:
+                correct -= 1
+                print("\nNo. Please try again.\n")
+            case 2:
+                correct -= 1
+                print("\nWrong. Try once more.\n")
+            case 3:
+                correct -= 1
+                print("\nDon't give up!\n")
+            case 4:
+                correct -= 1
+                print("\nNo. Keep trying.\n")
+            case _:
+                correct -= 1
+                print("\nInvalid Input. Please try again\n")
 
-while True:
-    num2=factorial(int(input("Enter a number to calculate its factorial:\n")))
 
-    print(f"this is the factorial:{num2}\n")
-    
-    ans=input("Do you want to continue:\n")
-    
-    if ans=='n' or ans=="N":
-        break
+for i in range (10):
+    rand = random.randint(1, 4) 
+    num1 = random.randint(1, 10)  
+    num2 = random.randint(1, 10)  
+    result=multiplication( num1,  num2)
+    ans = int(input((f"\nHow much is {num1} times {num2}?\n ")))
+    check_ans(rand, result, ans)
+
+
+percent: float = calculate_percentage()
+print(f"Your score is {correct}/{num_ques} which is {percent:.1%}\n.")
+print("\nThank you for participating.\n")
+correct = num_ques
