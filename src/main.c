7adce9eed8256769d2f2a44bb11953b4b23f7974
Amarusinggithub <loveamar285@gmail.com>
2026-02-23 09:46:41 -0500

    changes

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 /*
 Author: Amar Campbell- 2501977
+Date: 15/02/2026
+Description: Practice test
 */
 
 #include <stdio.h>
@@ -7,97 +9,28 @@ Author: Amar Campbell- 2501977
 #include <stdlib.h>
 #include <time.h>
 
+void populate();
+int findTotal();
 
-float calculatePercentage(int * correct, int numQues);
-int generateRand(int max, int min);
-int multiplication(int num1,int num2);
-void checkAns(int random, int result, int ans,int* correct);
-
+int arr[5];
 
 int main (){
-    srand(time(0));
-    int num1, num2,ans,result,random,numQues=10, correct=numQues;
-
-for (int i=0; i<10;i++){
-
-random=generateRand(4,1);
-num1= generateRand(10,1);
-num2=generateRand(10,1);
-result=multiplication( num1,  num2);
-
-printf("\nHow much is %d times %d?\n ", num1, num2);
-scanf("%d", &ans);
-
-checkAns( random, result, ans,&correct);
-    
-}
-
-float percent= calculatePercentage(&correct,  numQues);
-printf("Your score is %d/%d which is %.1f%%\n.",correct, numQues, percent);
-printf("\nThank you for participating.\n");
-correct=numQues;
-
+srand(time(0));
+populate();
+printf("This the sum of the array: %d", findTotal());
 return 0;
 }
 
-
-int generateRand(int max, int min){
-    return rand()%(max-min+1)+min;
-}
-
-int multiplication(int num1,int num2){
-    return num1 * num2;
-}
-
-void checkAns(int  random, int  result, int  ans,int* correct){
-    if(result==ans){
-    switch(random){
-        case 1:
-        printf("Very good!");
-        break;
-        case 2:
-        printf("\nExcellent!\n");
-        break;
-        case 3:
-        printf("\nNice work!\n");
-        break;
-        case 4:
-        printf("\nKeep up the good work!\n");
-        break;
-        default:
-            printf("\nVery good!\n");
-            break;
-    }
-
-}else{
-
-    switch(random){
-        case 1:
-        *correct--;
-        printf("\nNo. Please try again.\n");
-        break;
-        case 2:
-        *correct--;
-        printf("\nWrong. Try once more.\n");
-        break;
-        case 3:
-        *correct--;
-        printf("\nDon't give up!\n");
-        break;
-        case 4:
-        *correct--;
-        printf("\nNo. Keep trying.\n");
-        break;
-        default:
-        *correct--;
-        printf("\nInvalid Input. Please try again\n");
-        break;
+void populate(){
+    for ( int i=0; i<5;i++){
+        arr[i]= rand()%(21-11+1)+11;
     }
 }
+
+int findTotal(){
+    int sum;
+    for(int i=0;i<5;i+=){
+        sum+=arr[i];
+    }
+    return sum;
 }
-
-float calculatePercentage(int * correct, int numQues){
-return (*correct/ numQues)*100;
-}
-
-
--- src/main.py
+++ src/main.py
@@ -1,63 +1,267 @@
 """
-Author: Amar Campbell- 2501977
+Name: Amar Campbell - 2501977
+Date: 17/02/2026
+Description: lab test 1
 """
 
 import random
 
-num_ques = 10
-correct = num_ques
-
-def multiplication( num1:int, num2:int):
-    return num1 * num2
-
-
-def calculate_percentage( ) -> float:
-    return correct / num_ques
+# variables
+arr_of_things_bought = []
+arr_of_the_cost_things_bought = []
+did_select_seat = False
+did_select_destination = False
 
 
-def check_ans(  rand:int,   result:int,   ans:int):
-    global correct
-    if result==ans :
-        match rand:
+def select_destination():
+    """
+    This function prompts the user to select their destination
+    and then return the base price
+    """
+    invalid = True
+    while invalid:
+        invalid = False
+        print("\n1. New York.\n")
+        print("2.  Toronto.\n")
+        print("3.  London.\n")
+        print("4.  Miami.\n")
+        option = int(input("\nPlease select your base destination: (1,2,3,4):\n"))
+
+        match option:
             case 1:
-                print("Very good!")
+                return 45000
+
             case 2:
-                print("\nExcellent!\n")
+                return 35000
             case 3:
-                print("\nNice work!\n")
+                return 75000
+
             case 4:
-                print("\nKeep up the good work!\n")
+                return 40000
+
             case _:
-                print("\nVery good!\n")
+                invalid = True
+                print("Invalid Input. Please try Again.")
+
+
+def choose_seat_class():
+    """
+    This function prompts the user to select the seat upgrade
+    """
+    invalid = True
+    while invalid:
+        invalid = False
+        print("\n1. Economy.\n")
+        print("2.  Business.\n")
+        print("3.  First Class.\n")
+        option = int(input("\nPlease select a seat cost: (1,2,3):\n"))
+
+        match option:
+            case 1:
+                return 10
+
+            case 2:
+                return 20000
+            case 3:
+                return 40000
+            case _:
+                invalid = True
+                print("Invalid Input. Please try Again.")
+
+
+def add_extra_luggage():
+    """
+    This function prompts the user for the number of extra
+    luggage and return the cost
+    """
+    invalid = True
+    while invalid:
+        invalid = False
+        number_of_luggages = int(
+            input(
+                "\nPlease enter the number of extra luggage (the maximum is three (3)):\n"
+            )
+        )
+        if number_of_luggages < 0 or number_of_luggages > 3:
+            invalid = True
+            continue
+
+        match number_of_luggages:
+            case 0:
+                return 0.00
+            case 1:
+                return 0.00
+            case 2:
+                return 7000
+            case 3:
+                return 14000
+
+
+def select_meal():
+    """
+    This function prompts the user to select their
+    meal plan and return the cost
+    """
+    invalid = True
+    while invalid:
+        invalid = False
+        print("\n1. Standard.\n")
+        print("2.  Vegetarian.\n")
+        print("3.  Vegan.\n")
+        print("4.  Premium.\n")
+        option = int(input("\nPlease your meal plan: (1,2,3,4):\n"))
+
+        match option:
+            case 1:
+                return 0.00
+
+            case 2:
+                return 2000
+            case 3:
+                return 2500
+            case 4:
+                return 4000
+            case _:
+                invalid = True
+                print("Invalid Input. Please try Again.")
+
+
+def checkout(subtotal):
+    """
+    This the function that calculates the total cost of all items and print the receipt
+    """
+    # local variables
+    TAX = 0.12
+    DISCOUNT = 0.07
+    FLAT_FEE = 6000
+    total = subtotal
+    if not did_select_destination or not did_select_seat:
+        print("You must select a seat price and destination before checkout")
+        return False
+
+    if total > 100000:
+        total = total - (total * DISCOUNT)
+
+    # add tax and flat fee
+    total = total + (TAX * total) + FLAT_FEE
+
+    print("RECIEPT")
+    print("Cost      Price")
+    for i in range(len(arr_of_the_cost_things_bought)):
+        print(f"{arr_of_things_bought[i]}    ${arr_of_the_cost_things_bought[i]}\n")
+
+    # checks if user won a free flight
+    if random.randint(1, 50) == 25:
+        total = 0.00
+        print(" You won free flight chance")
+        print(f" Your final total is ${total} ")
     else:
-        match rand:
+        print(f" Your final total is ${total} ")
+
+    return True
+
+
+def main():
+    """
+    This is the main function that runs the entire program
+    """
+    global did_select_destination, did_select_seat
+    while True:
+        print("Welcome to The Airline Ticket Reservation System.\n")
+        print("\n1.Select Destination.\n")
+        print("2.  Choose Seat Class.\n")
+        print("3.  Add Extra Luggage.\n")
+        print("4.  Select Meal.\n")
+        print("5.  Checkout.\n")
+        print("6.  Exit.\n")
+        option = int(input("\nSelect an option (1,2,3,4,5,6):\n"))
+
+        match option:
             case 1:
-                correct -= 1
-                print("\nNo. Please try again.\n")
+                # This adds base price to array
+                did_select_destination = True
+                base_price: float = select_destination()
+                match base_price:
+                    case 45000:
+                        arr_of_the_cost_things_bought.append(base_price)
+                        arr_of_things_bought.append("New York")
+                    case 35000:
+                        arr_of_the_cost_things_bought.append(base_price)
+                        arr_of_things_bought.append("Toronto")
+                    case 75000:
+                        arr_of_the_cost_things_bought.append(base_price)
+                        arr_of_things_bought.append("London")
+                    case 40000:
+                        arr_of_the_cost_things_bought.append(base_price)
+                        arr_of_things_bought.append("Miami ")
+                    case _:
+                        print("Invalid Destination cost.")
             case 2:
-                correct -= 1
-                print("\nWrong. Try once more.\n")
+                # This adds seat  class upgrade cost to array
+                did_select_seat = True
+                seat_class_cost: float = choose_seat_class()
+                match seat_class_cost:
+                    case 10:
+                        arr_of_the_cost_things_bought.append(seat_class_cost)
+                        arr_of_things_bought.append("Economy")
+                    case 20000:
+                        arr_of_the_cost_things_bought.append(seat_class_cost)
+                        arr_of_things_bought.append("Business")
+                    case 40000:
+                        arr_of_the_cost_things_bought.append(seat_class_cost)
+                        arr_of_things_bought.append("First Class")
+                    case _:
+                        print("Invalid seat cost")
             case 3:
-                correct -= 1
-                print("\nDon't give up!\n")
+                # This add cost of the extra luggage arrays
+                cost_of_luggage = add_extra_luggage()
+                match cost_of_luggage:
+                    case 0.00:
+                        arr_of_the_cost_things_bought.append(cost_of_luggage)
+                        arr_of_things_bought.append("Extra Luggage (0-1 bags)")
+                    case 7000:
+                        arr_of_the_cost_things_bought.append(cost_of_luggage)
+                        arr_of_things_bought.append("Extra Luggage (2 bags)")
+                    case 14000:
+                        arr_of_the_cost_things_bought.append(cost_of_luggage)
+                        arr_of_things_bought.append("Extra Luggage (3 bags)")
+                    case _:
+                        arr_of_the_cost_things_bought.append(0.00)
+                        arr_of_things_bought.append("No Extra Luggage")
+
             case 4:
-                correct -= 1
-                print("\nNo. Keep trying.\n")
+                # This returns class upgrade cost  adds to arrays
+                meal_cost = select_meal()
+                match meal_cost:
+                    case 0.00:
+                        arr_of_the_cost_things_bought.append(meal_cost)
+                        arr_of_things_bought.append("Standard Meal")
+                    case 2000:
+                        arr_of_the_cost_things_bought.append(meal_cost)
+                        arr_of_things_bought.append("Vegetarian Meal")
+                    case 2500:
+                        arr_of_the_cost_things_bought.append(meal_cost)
+                        arr_of_things_bought.append("Vegan Meal")
+                    case 4000:
+                        arr_of_the_cost_things_bought.append(meal_cost)
+                        arr_of_things_bought.append("Premium Meal")
+                    case _:
+                        arr_of_the_cost_things_bought.append(0.00)
+                        arr_of_things_bought.append("No Meal")
+
+            case 5:
+                # This calculates and prints the final cost and end loop if successful.
+                subtotal = sum(arr_of_the_cost_things_bought)
+                successful: bool = checkout(subtotal)
+
+                if successful:
+                    # Exit the program on successful checkout
+                    break
+            case 6:
+                # Exit the program
+                break
             case _:
-                correct -= 1
-                print("\nInvalid Input. Please try again\n")
+                print("Invalid Input. Please try Again.")
 
-
-for i in range (10):
-    rand = random.randint(1, 4) 
-    num1 = random.randint(1, 10)  
-    num2 = random.randint(1, 10)  
-    result=multiplication( num1,  num2)
-    ans = int(input((f"\nHow much is {num1} times {num2}?\n ")))
-    check_ans(rand, result, ans)
-
-
-percent: float = calculate_percentage()
-print(f"Your score is {correct}/{num_ques} which is {percent:.1%}\n.")
-print("\nThank you for participating.\n")
-correct = num_ques
+if __name__ == "__main__":
+    main()
