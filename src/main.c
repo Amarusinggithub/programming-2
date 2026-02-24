2fdbbc6b13d85c47510c351abaed053a369e5a22
Amarusinggithub <loveamar285@gmail.com>
2026-02-24 08:51:24 -0500

    largest number in an array

--- src/main.c
+++ src/main.c
@@ -1,19 +1,63 @@
 /*
 Author: Amar Campbell- 2501977
 Date: 15/02/2026
-Description: Practice test
+Description: worksheet 5 question 2
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-
-int arr=[[1, 2, 3, 4, 5],
-    [2, 4, 6, 8, 10],
-    [20, 10, 5, 3, 1],
-    [3, 6, 9, 12, 15]]
+void displaySmallest(int arr[10]);
+void displayLargest(int arr[10]);
 
 int main (){
+srand(time(0));
+
+int arr[10];
+
+for(int i=0; i<10;i++){
+    arr[i]=rand()%(25-10+1)+10;
+    printf("%d \n",arr[i]);
+}
+
+
+displaySmallest(arr);
+displayLargest(arr);
 
 return 0;
 }
+
+void displaySmallest(int arr[10]){
+int smallest=arr[0];
+for(int i=0; i<10;i++){
+    if(arr[i]<smallest){
+        smallest=arr[i];
+    }
+}
+
+    printf("\n%d the smallest number.\n", smallest);
+
+}
+
+
+void displayLargest(int arr[10]){
+int largest=arr[0];
+
+for(int i=0; i<10;i++){
+
+if(arr[i]>largest){
+        largest=arr[i];
+    }    }
+
+        printf("\n %d the largest number.\n", largest);
+
+}
+
+
+
+
+
+
+
 
