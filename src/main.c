ede49ec352b22a58f22a2f7ce20a0fec4d84d098
Amarusinggithub <loveamar285@gmail.com>
2026-02-24 09:24:05 -0500

    worksheet 5 question 3

--- src/main.c
+++ src/main.c
@@ -1,61 +1,78 @@
 /*
 Author: Amar Campbell- 2501977
 Date: 15/02/2026
-Description: worksheet 5 question 2
+Description: worksheet 5 question 3
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-void displaySmallest(int arr[],int size);
-void displayLargest(int arr[],,int size);
+int countCustomers(float arr[],int size);
+float calculateTotal(float arr[],int size);
+float findLowest(float arr[],int size);
+void addInterest(float arr[],int size);
 
 int main (){
 srand(time(0));
 
-int arr[10];
+float arr[25];
 
-for(int i=0; i<10;i++){
-    arr[i]=rand()%(25-10+1)+10;
-    printf("%d \n",arr[i]);
+for(int i=0; i<25;i++){
+    arr[i]=rand()%(50000-1000+1)+1000;
+    printf("$%.2f \n",arr[i]);
 }
 
+printf("\nThere are  %d, with balance above $25,000\n",countCustomers(arr, 25));
+printf("\n This is the total of the customers balance: $%.2f\n",calculateTotal( arr,25));
+printf("\n This is the lowest balance: $%.2f",findLowest( arr,25));
+addInterest( arr,25);
 
-displaySmallest(arr,10);
-displayLargest(arr,10);
+printf("\n");
+for(int i=0; i<25;i++){
+    printf("$%.2f \n",arr[i]);
+}
 
 return 0;
 }
 
-void displaySmallest(int arr[],int size){
-int smallest=arr[0];
-for(int i=0; i<size;i++){
-    if(arr[i]<smallest){
-        smallest=arr[i];
+
+int countCustomers(float arr[],int size){
+    int count=0;
+    for(int i=0;i<size;i++){
+        if(arr[i]>25000){
+            count++;
+        }
+    }
+    return count;
+}
+
+float calculateTotal(float arr[],int size){
+    float sum=0;
+    for (int i=0;i<size;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+float findLowest(float arr[],int size){
+    float lowest=arr[0];
+    for(int i=0; i<size;i++){
+        if(arr[i]<lowest){
+            lowest=arr[i];
+        }
+    }
+
+    return lowest;
+}
+
+void addInterest(float arr[],int size){
+    const float INTEREST=0.05;
+    int interest;
+    for(int i=0; i<size; i++){
+        interest= arr[i]*INTEREST;
+        arr[i]+=interest;
     }
 }
-
-    printf("\n%d the smallest number.\n", smallest);
-
-}
-
-
-void displayLargest(int arr[],,int size){
-int largest=arr[0];
-
-for(int i=0; i<size;i++){
-
-if(arr[i]>largest){
-        largest=arr[i];
-    }    }
-
-        printf("\n %d the largest number.\n", largest);
-
-}
-
-
-
 
 
 
