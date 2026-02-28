43406c6981010adffa83951640e6270f4f8c800b
Amarusinggithub <loveamar285@gmail.com>
2026-02-28 17:58:07 -0500

    worksheet 5 question 8 of tutorial questions

--- src/main.c
+++ src/main.c
@@ -1,78 +1,63 @@
 /*
 Author: Amar Campbell- 2501977
 Date: 15/02/2026
-Description: worksheet 5 question 3
+Description: worksheet 5 question 8 of tutorial questions
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define ROW 5
+#define COLUMN 6
 
-int countCustomers(float arr[],int size);
-float calculateTotal(float arr[],int size);
-float findLowest(float arr[],int size);
-void addInterest(float arr[],int size);
+
+
 
 int main (){
-srand(time(0));
 
-float arr[25];
+int arr[ROW][COLUMN] = {
+    {1, 2, 3, 4, 5},
+    {2, 4, 6, 8, 10},
+    {20, 10, 5, 3, 1},
+    {3, 6, 9, 12, 15}
+};
 
-for(int i=0; i<25;i++){
-    arr[i]=rand()%(50000-1000+1)+1000;
-    printf("$%.2f \n",arr[i]);
+for (int r=0;r<ROW-1;r++){
+    int sum=0;
+    for(int c=0;c<COLUMN-1;c++){
+        sum+=arr[r][c];
+    }
+            arr[r][5]=sum;
 }
 
-printf("\nThere are  %d, with balance above $25,000\n",countCustomers(arr, 25));
-printf("\n This is the total of the customers balance: $%.2f\n",calculateTotal( arr,25));
-printf("\n This is the lowest balance: $%.2f",findLowest( arr,25));
-addInterest( arr,25);
+for(int c=0;c<COLUMN-1;c++){
+    int sum=0;
+    for(int r=0;c<ROW-1; r++){
+        sum+=arr[r][c];
+    }
+    arr[4][c]=sum;
+}
 
-printf("\n");
-for(int i=0; i<25;i++){
-    printf("$%.2f \n",arr[i]);
+int grandtotal=0;
+for(int c=0;c<COLUMN;c++){
+    grandtotal+=arr[4][c];
+}
+
+arr[4][5]=grandtotal;
+
+
+for (int r=0;r<ROW-1;r++){
+    for(int c=0;c<COLUMN-1;c++){
+        printf("%5d",arr[r][c]);
+    }
+    printf("\n");
 }
 
 return 0;
 }
 
 
-int countCustomers(float arr[],int size){
-    int count=0;
-    for(int i=0;i<size;i++){
-        if(arr[i]>25000){
-            count++;
-        }
-    }
-    return count;
-}
 
-float calculateTotal(float arr[],int size){
-    float sum=0;
-    for (int i=0;i<size;i++){
-        sum+=arr[i];
-    }
-    return sum;
-}
-float findLowest(float arr[],int size){
-    float lowest=arr[0];
-    for(int i=0; i<size;i++){
-        if(arr[i]<lowest){
-            lowest=arr[i];
-        }
-    }
-
-    return lowest;
-}
-
-void addInterest(float arr[],int size){
-    const float INTEREST=0.05;
-    int interest;
-    for(int i=0; i<size; i++){
-        interest= arr[i]*INTEREST;
-        arr[i]+=interest;
-    }
-}
 
 
 
