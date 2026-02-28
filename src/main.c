2929f56f9e4aa3a3dd1bad3262f87588564baa7f
Amarusinggithub <loveamar285@gmail.com>
2026-02-28 18:14:15 -0500

    bubble sort

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /*
 Author: Amar Campbell- 2501977
 Date: 15/02/2026
-Description: worksheet 5 question 8 of tutorial questions
+Description: worksheet 5 
 */
 
 #include <stdio.h>
@@ -15,43 +15,29 @@ Description: worksheet 5 question 8 of tutorial questions
 
 int main (){
 
-int arr[ROW][COLUMN] = {
-    {1, 2, 3, 4, 5},
-    {2, 4, 6, 8, 10},
-    {20, 10, 5, 3, 1},
-    {3, 6, 9, 12, 15}
-};
+int arr[8] = {5, 12, 3, 8, 21, 7, 14, 2};
 
-for (int r=0;r<ROW-1;r++){
-    int sum=0;
-    for(int c=0;c<COLUMN-1;c++){
-        sum+=arr[r][c];
+for(int i=0;i<7;i++){
+    printf("%3d,",arr[i]);
+}
+
+for(int i=0;i<7;i++){
+    for(int j=i+1;j<7;j++){
+    if(arr[i]>arr[j]){
+        int temp=arr[j];
+        arr[j]=arr[i];
+        arr[i]=temp;
+    }        
     }
-            arr[r][5]=sum;
+}
+    printf("\nSorted:\n");
+
+for(int i=0;i<7;i++){
+    printf("\n%3d,",arr[i]);
 }
 
-for(int c=0;c<COLUMN-1;c++){
-    int sum=0;
-    for(int r=0;c<ROW-1; r++){
-        sum+=arr[r][c];
-    }
-    arr[4][c]=sum;
-}
-
-int grandtotal=0;
-for(int c=0;c<COLUMN;c++){
-    grandtotal+=arr[4][c];
-}
-
-arr[4][5]=grandtotal;
 
 
-for (int r=0;r<ROW-1;r++){
-    for(int c=0;c<COLUMN-1;c++){
-        printf("%5d",arr[r][c]);
-    }
-    printf("\n");
-}
 
 return 0;
 }
