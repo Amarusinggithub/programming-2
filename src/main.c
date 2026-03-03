0c954e0911d0dc547e54b3dfe2b1e6897df2c0d7
Amarusinggithub <loveamar285@gmail.com>
2026-03-03 09:24:15 -0500

    worksheet 6 question 2

--- src/main.c
+++ src/main.c
@@ -1,67 +1,39 @@
 /*
 Author: Amar Campbell- 2501977
 Date: 15/02/2026
-Description: worksheet 5 
+Description: worksheet 6
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 #include <time.h>
-#define ROW 5
-#define COLUMN 6
 
-
-void binarySearch(int num);
-
+void checkEven(int *empIds,int *even,int size);
 
 int main (){
 srand(time(0));
+int even=0;
+int empIds [8];
 
-
-int arr[5];
-
-for(int i=0;i<4;i++){
-    arr[i]=rand()%(20-1+1)+1;
-    printf("%3d,",arr[i]);
+for(int i=0; i<7;++i){
+    empIds[i]=rand( )%(100-50+1)+50;
+    printf("%3d", empIds[i]);
 }
 
-/*for(int i=0; i<7;i++){
-   // int min_idx=i;
-    for(int j=i+1;j<7;j++){
-    
-        if(arr[min_idx]>arr[j]){
-            min_idx=j;
-        }
+checkEven(empIds ,&even, 8);
 
-        if(arr[j]>arr[i]){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-        }
-        
-    }*/
-
-    int temp=arr[0];
-    arr[0]=arr[4];
-    arr[4]=temp;
-
-printf("\n");
-printf("\n");
-
-for(int i=0;i<4;i++){
-    printf("%3d,",arr[i]);
-}
-
+printf("\nThere are %d employee id that are even.", even);
     return 0;
 }
 
 
-void binarySearch(int num){
-
+void checkEven(int *empIds ,int *even,int size){
+    for(int i=0; i<size-1; ++i){
+        if(*(empIds+i)%2==0){
+            (*even)++;
+        }
+    }
 }
-
-
-
 
 
 
